4f02da539671b1c457b297f4cd62f633143daf8c
isabella-lopez02 <isabellalopezarango@gmail.com>
2025-03-10 17:51:16 -0500

    ne2

---
+++ Banco.js
@@ -0,0 +1,7 @@
+const userName = document.querySelector('.name').value
+const userEmail = document.querySelector('.Email').value
+const userPassword = document.querySelector('.password').value
+
+
+
+//Sapoooooo
